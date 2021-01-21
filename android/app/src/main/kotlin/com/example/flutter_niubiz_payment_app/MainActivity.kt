2ceea28c0f82542ee6c4b40433298a6e7f103392
Joseph Quiroz <me@josephquiroz.com>
2021-01-21 12:08:43 -0500

    Implementando canal de comunicación para Android

--- android/app/src/main/kotlin/com/example/flutter_niubiz_payment_app/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/flutter_niubiz_payment_app/MainActivity.kt
@@ -1,6 +1,23 @@
 package com.example.flutter_niubiz_payment_app
 
+import androidx.annotation.NonNull
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodChannel
 
-class MainActivity: FlutterActivity() {
+class MainActivity : FlutterActivity() {
+    private val CHANNEL = "samples.flutter.dev/mychannel"
+
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+        MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL).setMethodCallHandler { call, result ->
+            // Note: this method is invoked on the main thread.
+            if (call.method == "myMethod") {
+                // TODO: Implementar método
+                result.success("Success")
+            } else {
+                result.notImplemented()
+            }
+        }
+    }
 }
