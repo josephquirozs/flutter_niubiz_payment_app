2b152f4a4eee796082a3db542911229f80792bd0
Joseph Quiroz <me@josephquiroz.com>
2021-01-22 18:46:00 -0500

    Completando el resto de propiedades del formulario de pago

--- lib/payment_form_properties.dart
+++ lib/payment_form_properties.dart
@@ -2,4 +2,13 @@ final paymentFormProperties = {
   'channel': 'mobile',
   'countable': true,
   'securityToken': null,
+  'merchant': '456879852',
+  'purchaseNumber': '1790',
+  'amount': '15.22',
+  'endPointURL': 'https://apisandbox.vnforappstest.com/',
+  'certificateHost': 'apisandbox.vnforappstest.com',
+  'certificatePin': 'sha256/lmxiL6uol7hb4UwDxtk2qbF2lBnJc7zqZRT6sFfYWEE=',
+  'registerName': 'Juan',
+  'registerLastname': 'Perez',
+  'registerEmail': 'jperez@test.com',
 };
