d8a61507acac980074a5d44ff04b78cab9a032fc
Joseph Quiroz <me@josephquiroz.com>
2021-01-25 20:42:41 -0500

    Implementando la respuesta del formulario de pago

--- android/app/src/main/kotlin/com/example/flutter_niubiz_payment_app/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/flutter_niubiz_payment_app/MainActivity.kt
@@ -1,7 +1,7 @@
 package com.example.flutter_niubiz_payment_app
 
 import android.content.Intent
-import android.widget.Toast
+import android.util.Log
 import androidx.annotation.NonNull
 import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
@@ -9,6 +9,7 @@ import io.flutter.plugin.common.MethodChannel
 import lib.visanet.com.pe.visanetlib.VisaNet
 
 class MainActivity : FlutterActivity() {
+    private val TAG = "Main"
     private val CHANNEL = "samples.flutter.dev/mychannel"
 
     override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
@@ -40,10 +41,18 @@ class MainActivity : FlutterActivity() {
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
-        if (requestCode == VisaNet.VISANET_AUTHORIZATION) {
-            // TODO: Implementar respuesta del formulario
-        } else {
-            Toast.makeText(applicationContext, "Unauthorized", Toast.LENGTH_LONG).show()
+        when (requestCode) {
+            VisaNet.VISANET_AUTHORIZATION -> {
+                val response: String?
+                if (resultCode == RESULT_OK) {
+                    response = data?.extras?.getString("keySuccess")
+                    Log.i(TAG, "Payment success $response")
+                } else {
+                    response = data?.extras?.getString("keyError")
+                    Log.i(TAG, "Payment error $response")
+                }
+            }
+            else -> Log.i(TAG, "Request code not implemented")
         }
     }
 }
